bda17b6b5f464c9db01d3f12bd90adc27587a386
Jason Fung <solidzoro@live.com>
2020-10-08 19:59:05 +0800

    fix: fixed 篷 U+7BF7 error
    Closes: #26

--- tools/build.sh
+++ tools/build.sh
@@ -4,8 +4,9 @@ cd "$(dirname "$0")" || exit
 
 rm -f ./*.ttf
 
-java -jar ./bin/BitsNPicas.jar convertbitmap -f ttf -o ./zpix.ttf ../src/zpix.bdf
+#java -jar ./bin/BitsNPicas.jar convertbitmap -f ttf -o ./zpix.ttf ../src/zpix.bdf
+java -jar ./bin/BitsNPicas.jar convertbitmap -f ttf -o ./zpix.ttf ../src/zpix.sfd
 
-./minify.py
+# ./minify.py
 
 mv -f ./zpix.ttf ../dist/zpix.ttf
