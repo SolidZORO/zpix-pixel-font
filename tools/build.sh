77f57f5121fe219f7a067091833bfe112e85b4a0
Jason Fung <solidzoro@live.com>
2020-10-08 21:10:55 +0800

    refactor: change Font Weight

--- tools/build.sh
+++ tools/build.sh
@@ -4,9 +4,12 @@ cd "$(dirname "$0")" || exit
 
 rm -f ./*.ttf
 
-#java -jar ./bin/BitsNPicas.jar convertbitmap -f ttf -o ./zpix.ttf ../src/zpix.bdf
 java -jar ./bin/BitsNPicas.jar convertbitmap -f ttf -o ./zpix.ttf ../src/zpix.sfd
-
-# ./minify.py
-
+./minify.py
 mv -f ./zpix.ttf ../dist/zpix.ttf
+
+
+rm -f ./*.bdf
+
+java -jar ./bin/BitsNPicas.jar convertbitmap -f bdf -o ./zpix.bdf ../src/zpix.sfd
+mv -f ./zpix.bdf ../dist/zpix.bdf
